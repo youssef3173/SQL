e56bb368ffaef5b306088813c574e0cb4f2e375a
Youssef <youssefbouzian71@gmail.com>
2022-10-26 10:21:03 +0200

    Exercice 2
    Change the name of a column

---
+++ Ex2.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+
+db_name = "Ex1.db"
+
+
+
+mydb = sqlite3.connect( db_name )
+mycursor = mydb.cursor()
+
+
+
+# Change the name of the column the data returns to Band Name
+mycursor.execute(""" SELECT name AS 'Band Name'
+					FROM bands
+					;""")
+
+list_bands = mycursor.fetchall() 
+for bname in list_bands:
+	print( bname)
+
+mydb.commit()
+mydb.close()
+print( "Done" )
