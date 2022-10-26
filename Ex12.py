de42a9fa9deed5979e10c4f3d83034f1a5325830
Youssef <youssefbouzian71@gmail.com>
2022-10-26 14:21:50 +0200

    Exercice 11

---
+++ Ex12.py
@@ -0,0 +1,23 @@
+import sqlite3
+import os
+
+
+db_name = "Ex1.db"
+
+mydb = sqlite3.connect( db_name )
+mycursor = mydb.cursor()
+
+
+# Get the number of Songs for each Band:
+mycursor.execute( """SELECT albums.name AS Name, albums.release_year, MAX(songs.length) AS Duration FROM albums
+					INNER JOIN songs ON albums.id = songs.album_id 
+					GROUP BY albums.name
+					ORDER BY albums.id
+					;""")
+res = mycursor.fetchall()
+for r in res:
+	print( r)
+
+mydb.commit()
+mydb.close()
+print( "Done" ) 
